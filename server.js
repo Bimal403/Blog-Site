4524707da2e86dfb824c32e079677cbbc6c6ac2e
BimalTimsina <bimaltimsina4@gmail.com>
2024-01-22 23:23:22 +0545

    display area for article is done

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const articleRouter = require("./routes/articles");
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use("/articles", articleRouter);
+app.get("/", (req, res) => {
+  const articles = [
+    {
+      title: "Test Article",
+      createdAt: new Date(),
+      description: "Test description",
+    },
+    {
+      title: "Test Article2",
+      createdAt: new Date(),
+      description: "Test description2",
+    },
+  ];
+  res.render("articles/", { articles: articles });
+});
+app.listen(3005);
